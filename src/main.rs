1510b7bdfa40af2d9c6a53ffc629dc4fc7aaf8d7
lookup-1922 <125456965+lookup-1922@users.noreply.github.com>
2024-04-22 17:55:36 +0900

    コマンドライン引数の取得の調整

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,34 @@
 mod utils;
 use utils::file::open_or_create_file;
-use utils::prime::sieve_of_eratosthenes;
 use utils::perfect_num::{is_mersenne_prime, is_perfect_number};
+use utils::prime::sieve_of_eratosthenes;
 
 use std::env;
 
 fn main() {
+    // コマンドライン引数を取得
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 {
-        eprintln!("Usage: {} <max_number>", args[0]);
+    // コマンドライン引数用の変数の宣言
+    let mut command_type:String;
+    let mut max_number: u32;
+
+    // コマンドライン引数の数を確認して値を変数に格納
+    if args.len() == 1 {
+        command_type = args[1];
+    } else if args.len() == 2 {
+        command_type = args[1];
+        max_number = match args[2].parse::<u32>() {
+            Ok(num) => num,
+            Err(_) => {
+                eprintln!("Error: Invalid number provided");
+                std::process::exit(1);
+            }
+        };
+    } else {
+        eprintln!("Usage: <command> <number>");
         std::process::exit(1);
     }
-
-    let max_number = match args[1].parse::<u32>() {
-        Ok(num) => num,
-        Err(_) => {
-            eprintln!("Error: Invalid number provided");
-            std::process::exit(1);
-        }
-    };
 
     let primes = sieve_of_eratosthenes(max_number);
     let mut file = open_or_create_file("primes.txt");
